9a5e726cf121820d09632a7364979ae47d0cb2a4
Ërick Ndava <rick.ndava@gmail.com>
2019-05-27 23:25:35 +0200

    learning more on joins

--- relational_data.R
+++ relational_data.R
@@ -55,3 +55,58 @@ j_right_join <- x %>%
 j_full_join <- x %>% 
   full_join(y, by="key")
   View(j_full_join)
+  
+#Duplicate keys
+x <- tribble(
+  ~key, ~val_x,
+  1, "x1",
+  2, "x2",
+  2, "x3",
+  1, "x4"
+)
+
+y <- tribble(
+  ~key, ~val_y,
+  1, "y1",
+  2, "y2"
+)
+
+left_join(x,y,by="key")
+
+#joining duplicate keys gives the Cartesian product
+x <- tribble(
+  ~key, ~val_x,
+  1, "x1",
+  2, "x2",
+  2, "x3",
+  3, "x4"
+)
+
+y <- tribble(
+  ~key, ~val_y,
+  1, "y1",
+  2, "y2",
+  2, "y3",
+  3, "y4"
+)
+
+left_join(x,y,by="key")
+
+#Defining the key column
+
+flights2 %>% 
+  left_join(weather)
+
+flights2 %>% 
+  left_join(planes, by="tailnum")
+
+flights2 %>% 
+  left_join(airports,c("dest"="faa"))
+
+#Exercises
+airports %>% 
+  semi_join(flights,c("faa"="dest")) %>% 
+  ggplot(aes(lon, lat))+
+  borders("state")+
+  geom_point()+
+  coord_quickmap()
